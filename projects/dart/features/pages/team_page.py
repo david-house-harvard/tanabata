f5a695f112e4626a30b5008480ab7758b59b45b3
Misha <suxx_2000@hotmail.com>
2017-10-13 11:54:03 +0300

    upd

--- projects/dart/features/pages/team_page.py
+++ projects/dart/features/pages/team_page.py
@@ -30,3 +30,30 @@ class TeamPage(CommonPage):
         everything_is_ok = el.value_of_css_property('opacity') > 0 and el.size.height > 5
         assert everything_is_ok
 
+    def check_logo_vpal(self):
+        self.check_elements(attr_name='logo-VPAL')
+
+    def check_sections(self):
+        if self.check_elements(attr_name='team-section'):
+            elements = self.find_all(attr_name='team-section')
+        assert len(elements) == 4
+
+    def check_vpal(self):
+        elements = self.find_all(attr_name='team-section')
+        assert elements[0]
+
+    def check_huit(self):
+        elements = self.find_all(attr_name='team-section')
+        assert elements[1]
+
+    def check_contract(self):
+        elements = self.find_all(attr_name='team-section')
+        assert elements[2]
+
+    def check_alumni(self):
+        elements = self.find_all(attr_name='team-section')
+        assert elements[3]
+
+    def check_aang_photo(self):
+        self.find(attr_name=)
+
